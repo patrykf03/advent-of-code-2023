65d71c87954ce41bd399dcd37b883e93934417f7
Patrykf03 <61631089+patrykf03@users.noreply.github.com>
2023-12-11 09:44:16 +0000

    memoize 8p2 in js for fun

--- day8/8-2.js
+++ day8/8-2.js
@@ -1,49 +1,62 @@
-const { once } = require('events');
-const fs = require('fs');
+import {nanomemoize as memoize} from 'nano-memoize';
+import fs from 'fs';
 
 // load input as string
 const taskInput = fs.readFileSync('input.txt', 'utf8');
 
-const originalMoves = taskInput.split('\n')[0];
+const moves = taskInput.split('\n')[0];
 const nodes = {};
-taskInput.split('\n').slice(2).forEach(nodeLine => {
+    taskInput.split('\n').slice(2).forEach(nodeLine => {
   const foundItems = nodeLine.split('=');
   const nodeKey = foundItems[0].trim();
   const nodeValue = [...foundItems[1].match(/(\w+)/g)];
   nodes[nodeKey] = nodeValue;
 });
 
-function applyMoves(moves, startingNodes) {
-  let currentNodes = startingNodes;
-  let stepCount = 0;
+function applyMovesSingle(startingNode) {
+  let currentNode = startingNode
+  let stepCount = 0
   for (const move of moves){
     stepCount++;
-    if (currentNodes.every(node => node[2] === 'Z')) {
-      break;
-    }
-    currentNodes = currentNodes.map(node => {
-      if (move === 'R') {
-        return nodes[node][1]
-      } else {
-        return nodes[node][0]
-      }
-    });
-  }
-  return [currentNodes, stepCount];
+    if (move === 'R') {
+      currentNode = nodes[currentNode][1]
+    } else {
+      currentNode = nodes[currentNode][0]
+    };
+  };
+  return [stepCount,currentNode];
 }
+
+const applyMovesSingleMemoized = memoize(applyMovesSingle)
+
+function applyMoves(startingNodes) {
+  let currentNodes = startingNodes;
+  let stepCount = 0;
+
+  for (const currentNode of currentNodes) {
+    let newResult;
+    newResult = applyMovesSingleMemoized(currentNode);
+    stepCount += newResult[0];
+    currentNodes[currentNodes.indexOf(currentNode)] = newResult[1];
+  };
+  stepCount = stepCount / startingNodes.length
+  return stepCount;
+}
+
+
 
 let totalSteps = 0;
 let currentNodes = Object.keys(nodes)
   .filter((key) => key[2] === "A")
-const oneCycleLength = originalMoves.length;
+const oneCycleLength = moves.length;
+
 
 while (!(currentNodes.every(node => node[2] == 'Z'))) {
-  [currentNodes, currentSteps] = applyMoves(originalMoves, currentNodes);
-  totalSteps += currentSteps;
-  if (totalSteps % (4000 * oneCycleLength) === 0) {
+  totalSteps += applyMoves(currentNodes);
+  if (totalSteps % (10000 * oneCycleLength) === 0) {
     console.log(totalSteps);
   }
 }
 
-console.log("FOUND")
-console.log(currentNodes)
+console.log("FOUND");
+console.log(totalSteps);
